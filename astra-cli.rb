fe841ce9e85dd075c187fb51fe6cdc8091b13cd6
Cedrick Lunven <cedrick.lunven@gmail.com>
2022-12-05 16:21:40 +0100

    Update astra-cli.rb

--- astra-cli.rb
+++ astra-cli.rb
@@ -1,16 +1,9 @@
 class AstraCli < Formula
   desc "Command Line Interface for DataStax Astra"
   homepage "https://awesome-astra.github.io/docs/pages/astra/astra-cli/"
-  url "https://github.com/datastax/astra-cli/releases/download/0.1/astra-cli-0.1-mac.tar.gz"
-  sha256 "a5ded836246cf775ab4068b79528b60c245f2f2a57d451f6dfe97b6b05622a64"
+  url "https://github.com/datastax/astra-cli/releases/download/0.2/astra-cli-0.2-homebrew.tar.gz"
+  sha256 "bbdbfca6d5ebcc8cb1cddb8a3bce41e11c7a18f6620d4bcc9a3e48ad51b1e218"
   license "Apache-2.0"
-
-  #depends_on "openjdk@11" # For DsBulk and PulsarShell
-  #depends_on "python@3.10" # For CqlShell
-
-  #bottle do
-  #  sha256 cellar: :any_skip_relocation, ventura: "98656a29e9c0282deb6f7e12b8f863453ad3c63d9149333042ce953f19944aa2"
-  #end
 
   def install
     libexec.install Dir["*"]
