76a8bd1697dbf063b75d52413f7e0f96b10444be
Cedrick Lunven <cedrick.lunven@gmail.com>
2022-12-09 14:34:35 +0100

    Update astra-cli.rb

--- astra-cli.rb
+++ astra-cli.rb
@@ -2,7 +2,7 @@ class AstraCli < Formula
   desc "Command Line Interface for DataStax Astra"
   homepage "https://awesome-astra.github.io/docs/pages/astra/astra-cli/"
   url "https://github.com/datastax/astra-cli/releases/download/0.2/astra-cli-0.2-homebrew.tar.gz"
-  sha256 "bbdbfca6d5ebcc8cb1cddb8a3bce41e11c7a18f6620d4bcc9a3e48ad51b1e218"
+  sha256 "9bf0debc56beb9c75df9e7b9d4404dabf9ec3aee617e20acbda8338361db1830"
   license "Apache-2.0"
 
   def install
