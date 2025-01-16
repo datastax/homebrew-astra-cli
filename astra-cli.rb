dd3a475265699e77d91b651340cc861e52ce1ebc
Cedrick Lunven <cedrick.lunven@gmail.com>
2025-01-16 13:49:36 +0100

    Update astra-cli.rb

--- astra-cli.rb
+++ astra-cli.rb
@@ -3,17 +3,16 @@ class AstraCli < Formula
   homepage "https://awesome-astra.github.io/docs/pages/astra/astra-cli/"
   url "https://github.com/datastax/astra-cli.git",
       tag:      "0.6",
-      #revision: "49e0d55a4f98f6f2544c0155e438d790731fb0b8"
       revision: "edce75c89ebfbc0b274e602cba5fb5bea7cc5ad8"
   license "Apache-2.0"
   head "https://github.com/datastax/astra-cli.git", branch: "main"
 
 bottle do
     root_url "https://github.com/datastax/astra-cli/releases/download/0.6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "149efe9618d4072f953bd3a50ea32265744c986607c9878889445373badc6886"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "149efe9618d4072f953bd3a50ea32265744c986607c9878889445373badc6886"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "149efe9618d4072f953bd3a50ea32265744c986607c9878889445373badc6886"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "149efe9618d4072f953bd3a50ea32265744c986607c9878889445373badc6886"
     sha256 cellar: :any_skip_relocation, sonoma:         "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
     sha256 cellar: :any_skip_relocation, ventura:        "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
     sha256 cellar: :any_skip_relocation, monterey:       "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
