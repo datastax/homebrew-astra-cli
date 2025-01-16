5bd205eef8a6fdcdb9a79bdf839404e089e637d6
Cedrick Lunven <cedrick.lunven@gmail.com>
2025-01-16 13:33:00 +0100

    Update astra-cli.rb

--- astra-cli.rb
+++ astra-cli.rb
@@ -2,27 +2,28 @@ class AstraCli < Formula
   desc "Command Line Interface for DataStax Astra"
   homepage "https://awesome-astra.github.io/docs/pages/astra/astra-cli/"
   url "https://github.com/datastax/astra-cli.git",
-      tag:      "0.5",
-      revision: "49e0d55a4f98f6f2544c0155e438d790731fb0b8"
+      tag:      "0.6",
+      #revision: "49e0d55a4f98f6f2544c0155e438d790731fb0b8"
+      revision: "edce75c89ebfbc0b274e602cba5fb5bea7cc5ad8"
   license "Apache-2.0"
   head "https://github.com/datastax/astra-cli.git", branch: "main"
 
 bottle do
-    root_url "https://github.com/datastax/astra-cli/releases/download/0.5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8567fc1e06074085857455c4f6da6f1fd2f14addba5ce0e1595adf2c6607304e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8567fc1e06074085857455c4f6da6f1fd2f14addba5ce0e1595adf2c6607304e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8567fc1e06074085857455c4f6da6f1fd2f14addba5ce0e1595adf2c6607304e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8567fc1e06074085857455c4f6da6f1fd2f14addba5ce0e1595adf2c6607304e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, ventura:        "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, monterey:       "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, catalina:       "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, mojave:         "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, sierra:         "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "764647e8037c37b4cd8f6b80e4cff46fa75df983cf5049cd917f5c536de5fcf8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7bef1d9413e167c417871c76bd770b4cafb6c3a058b0652c7cbf2ac57834b283"
+    root_url "https://github.com/datastax/astra-cli/releases/download/0.6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06615704c0e9167707b2bcc7be1bc42da7658bac51a6ab46d60a9920dc65445d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, ventura:        "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, monterey:       "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, catalina:       "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, mojave:         "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, sierra:         "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "dfdbc94a9f4eae1443740786df70bd217a1b1275a72ad51a038f71e1fe06b84c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b7c2e477e04b1fb774089864d98608a9ff7222859a0d10ea4e0279b56bf8e5cc"
   end
 
   def install
@@ -31,7 +32,7 @@ bottle do
     system "script/build"
     system "script/install", prefix
 
-    bash_completion.install "contrib/completion/astra-cli.bash" => "astra-cli"
+    bash_completion.install "astra-init.sh" => "astra-cli"
   end
 
   def caveats
