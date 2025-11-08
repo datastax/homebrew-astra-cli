302d7ae4662ff54f56769c1499a12411dea818d6
toptobes <kavinpg@gmail.com>
2025-11-07 23:24:19 -0600

    4μluh update

--- astra-canary.rb
+++ astra-canary.rb
@@ -1,52 +1,50 @@
 class AstraCanary < Formula
   desc "Command Line Interface for DataStax Astra (Canary version–not guaranteed to be stable)"
   homepage "https://docs.datastax.com/en/astra-cli"
-  version "1.0.0-rc.2"
+  version "1.0.0-rc.4"
   license "Apache-2.0"
   head "https://github.com/datastax/astra-cli.git", branch: "main"
 
-  conflicts_with "astra-cli", because: "'astra-cli' is the formula for the legacy v0.x Astra CLI. Please uninstall 'astra-cli' to continue." 
+  conflicts_with "astra-cli", because: "'astra-cli' is the formula for the legacy v0.x Astra CLI. \033[1mPlease uninstall 'astra-cli' to continue.\033[0m" 
 
   on_macos do
     on_arm do
       url "https://github.com/datastax/astra-cli/releases/download/v#{version}/astra-macos-arm64.tar.gz"
-      sha256 "b2d58e84051b85cbebfd30597e5fc96c46d1eddce195f68a610df8b64e8167cd"
+      sha256 "935d0e39b716f77fc8e44ca0aa804bb221d46f51c3e7e246a26313992e2225e6"
     end
   
     on_intel do
       url "https://github.com/datastax/astra-cli/releases/download/v#{version}/astra-macos-x86_64.tar.gz"
-      sha256 "8609324eab9ed9845d121bef9ce541552fac9b1fe29c46d878308485815239ed"
+      sha256 "4390ba8ef92e91564fdbacfa594ccc451e784130b8cea563727631fd83871b21"
     end
   end
   
   on_linux do
     on_arm do
       url "https://github.com/datastax/astra-cli/releases/download/v#{version}/astra-linux-arm64.tar.gz"
-      sha256 "ff600e22abfd77d8c4ae36e5a3175dca8ab5e4d8a58367b9e865b7726f63b7ac"
+      sha256 "37c7dde02c7b99d463119e4ff18b2df83ef84a687c4e05ffa5fd8eedbec38d1c"
     end
   
     on_intel do
       url "https://github.com/datastax/astra-cli/releases/download/v#{version}/astra-linux-x86_64.tar.gz"
-      sha256 "64abafad6d968a52a634ae3ba1eace699346d6442624edd9267491b15ed03e82"
+      sha256 "e55228abb7f41552e9ceafc04dbf8fa2bde02d589fbcac9d94ca9a2d532e10bb"
     end
   end
   
   def install
+    existing_astra = which("astra")
+
+    if existing_astra && !existing_astra.to_s.include?("Cellar")
+      odie "Found existing 'astra' installation at: #{existing_astra}\n" \
+           "Please uninstall it (or at least remove it from your PATH) before installing astra-canary."
+    end
+
     bin.install "bin/astra"
     generate_completions_from_executable bin/"astra", "compgen", shell_parameter_format: :none, shells: [:bash, :zsh]
   end
 
   def caveats
     <<~EOS
-      \033[1;34m-------------------------\033[0m
-      \033[1;34m  ENABLE AUTOCOMPLETION  \033[0m
-      \033[1;34m-------------------------\033[0m
-      Add the following to your shell profile (e.g. ~/.zprofile, ~/.bash_profile, etc.):
-      
-      \033[1;32meval "$(#{bin}/astra shellenv)"\033[0m
-
-      Once added, reload your shell or source your profile.
-
       \033[1;34m-------------------\033[0m
       \033[1;34m SPECIAL COMMANDS  \033[0m
       \033[1;34m-------------------\033[0m
@@ -58,7 +56,7 @@ class AstraCanary < Formula
   end
 
   test do
-    system bin"/astra", "config", "path"
+    system bin/"astra", "config", "path"
     system bin/"astra", "compgen"
   end
 end
